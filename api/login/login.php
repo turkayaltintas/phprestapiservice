aa87da6c6fdbf41995cdf761c560b0eb4a136206
Türkay ALTINTAŞ <taltintas@yandex.com>
2021-12-08 10:45:19 +0300

    s
    s

--- api/login/login.php
+++ api/login/login.php
@@ -6,7 +6,7 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../config/database.php';
-include_once '../objects/login.php';
+include_once '../objects/allClass.php';
 
 $data = json_decode(file_get_contents("php://input"));
 $database = new Database();
